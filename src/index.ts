a5ef01a1f708d0f1a411f14dbcac925a35e8db26
quantumquirkz <jhuomar3105@gmail.com>
2026-09-09 14:56:56 -0500

    feat: expose domain and fixture surface

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "./domain/index.js";
+export * from "./fixtures/index.js";
+export { InMemoryObservationRepository } from "./persistence/InMemoryObservationRepository.js";
+export { captureObservation } from "./service.js";
