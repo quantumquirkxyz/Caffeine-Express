e6f184a81556cc475f851d684577ab1c1b4a2163
quantumquirkz <jhuomar3105@gmail.com>
2026-09-10 01:33:52 -0500

    fix(web): surface runtime errors instead of rendering a blank screen

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+import React from 'react';
 import { registerRootComponent } from 'expo';
 import App from './src/App';
 
-registerRootComponent(App);
+class AppErrorBoundary extends React.Component {
+  state = { error: null };
+  static getDerivedStateFromError(error) {
+    return { error };
+  }
+  render() {
+    if (this.state.error) {
+      return React.createElement('pre', { style: { padding: 24, color: '#B42318', whiteSpace: 'pre-wrap' } }, String(this.state.error?.stack ?? this.state.error));
+    }
+    return this.props.children;
+  }
+}
+
+registerRootComponent(() => React.createElement(AppErrorBoundary, null, React.createElement(App)));
